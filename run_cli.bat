a55583a4e877475d3e840b5d1ece0aaca4a38ade
Fathoni <88326103+thoni08@users.noreply.github.com>
2026-04-18 12:47:44 +0700

    Replace batch files with PowerShell scripts for CLI and GUI execution with compiling before run added

--- run_cli.bat
+++
@@ -1,1 +0,0 @@
-java -cp src;lib/flatlaf-3.7.1.jar com.expertsystem.MainApp
---
+++ run_cli.ps1
@@ -0,0 +1,2 @@
+javac -cp ".;lib/flatlaf-3.7.1.jar" (Get-ChildItem -Recurse -Filter *.java | ForEach-Object { $_.FullName })
+java -cp src;lib/flatlaf-3.7.1.jar com.expertsystem.MainApp
