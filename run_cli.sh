81dedb69d4e7e666d821bfca5465651468c931f3
Fathoni <88326103+thoni08@users.noreply.github.com>
2026-04-18 12:47:51 +0700

    Add compilation step to run_cli.sh and run_gui.sh scripts

--- run_cli.sh
+++ run_cli.sh
@@ -1,1 +1,2 @@
+javac -cp ".:lib/flatlaf-3.7.1.jar" src/com/expertsystem/**/*.java src/com/expertsystem/view/gui/*.java src/com/expertsystem/*.java
 java -cp src:lib/flatlaf-3.7.1.jar com.expertsystem.MainApp
